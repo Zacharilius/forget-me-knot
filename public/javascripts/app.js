da2fbee15a068765c83228e071ecfcd754b8301f
Zacharilius <zabensley@gmail.com>
2017-02-04 12:26:06 -0800

    Initialized repository with Angular, Node, and Express.

---
+++ public/javascripts/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('forgetMeKnotApp',[]);
+
+app.controller('myController', function($scope, $http) {
+    $scope.data = [];
+    var request = $http.get('/data');
+    request.success(function(data) {
+        $scope.data = data;
+    });
+    request.error(function(data){
+        console.log('Error: ' + data);
+    });
+});
