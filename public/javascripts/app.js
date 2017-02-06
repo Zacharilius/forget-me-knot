97e2064f9e5a4e815daebb87cbd0754ca5841367
Zacharilius <zabensley@gmail.com>
2017-02-05 21:26:14 -0800

    Reminers can be deleted.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,46 @@
-var app = angular.module('forgetMeKnotApp',[]);
+var app = angular.module('forgetMeKnotApp', ['ngAnimate']);
 
 app.controller('myController', function($scope, $http) {
     $scope.reminders = [];
-    var request = $http.get('/data');
-    request.success(function(data) {
-        $scope.reminders = data;
-    });
-    request.error(function(data){
-        console.log('Error: ' + data);
-    });
+
+    $scope.addReminder = function() {
+        var reminder = {"title": "", "startReminderTime": "", "lastReminderTime": "", "remindEveryDays": 1};
+        $scope.reminders.unshift(reminder);
+    }
+
+    $scope.updateReminders = function() {
+        var request = $http.post('/api/reminders', $scope.reminders);
+        request.success(function(data) {
+            $scope.reminders = data;
+        });
+        request.error(function(data){
+            console.log('Error: ' + data);
+        });
+    }
+
+    $scope.deleteReminder = function($index, reminder) {
+        $scope.reminders.splice($index, 1);
+        $scope.$emit('reminderDeleted', reminder);
+
+        var request = $http.delete('/api/reminders/' + reminder.id);
+        request.success(function(data) {
+            console.log(data);
+            console.log('delete');
+        });
+        request.error(function(data){
+            console.log('Error: ' + data);
+        });
+    }
+
+    function init() {
+        var request = $http.get('/api/reminders');
+        request.success(function(data) {
+            $scope.reminders = data;
+        });
+        request.error(function(data){
+            console.log('Error: ' + data);
+        });
+    }
+
+    init();
 });
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,7 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-router.get('/data', function(req,res){
-    res.json([
+var reminders = [
         {"id": 1, "title": "Call Mom", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
         {"id": 2, "title": "Call Dad", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
         {"id": 3, "title": "Call Sister", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
@@ -20,5 +19,24 @@ router.get('/data', function(req,res){
         {"id": 7, "title": "Ha", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
         {"id": 8, "title": "Save $100", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
         {"id": 9, "title": "Call Brother", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5},
-        {"id": 10, "title": "Call Ex-wife", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5}]);
+        {"id": 10, "title": "Call Ex-wife", "startReminderTime": "2017-02-04T22:04:44.524Z", "lastReminderTime": "2017-02-05T22:04:44.524Z", "remindEveryDays": 5}
+    ];
+
+router.get('/api/reminders', function(req, res) {
+    res.json(reminders);
 });
+
+router.post('/api/reminders', function(req, res) {
+    var newReminder = req.body.reminders
+    console.log(newReminder);
+    res.json(reminders);
+});
+
+router.delete('/api/reminders/:id', function(req, res) {
+    var id = req.params.id;
+    reminders = reminders.filter(function(a){
+        return a['id'] != id;
+    });
+    res.json(reminders);
+});
+
